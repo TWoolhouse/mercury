974ff17c10a324005d181a5032536099559428a6
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-11-17 23:00:00 +0000

    Refactor file reading and error handling in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
+use std::{env, fs};
+
 fn main() {
-    let store = mercury::parse(include_str!("test.mcy").trim());
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Please provide a file path as a command line argument");
+        return;
+    }
+    let file_path = &args[1];
+
+    // Read the file contents
+    let file_contents = match fs::read_to_string(file_path) {
+        Ok(contents) => contents,
+        Err(error) => {
+            eprintln!("Error reading file: {}", error);
+            return;
+        }
+    };
+
+    let store = mercury::parse(file_contents.trim());
     match store {
         Ok(store) => {
             let mut values = mercury::evaluate(&store);
             values.sort_by_key(|x| x.0);
             println!("{:?}", values);
         }
-        Err(e) => println!("{}", e),
+        Err(e) => eprintln!("{}", e),
     }
 }
